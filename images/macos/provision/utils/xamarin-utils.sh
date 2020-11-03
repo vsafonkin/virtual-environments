17195f24b2c47f3e5256654378534d68c8455b73
Vladimir Safonkin <vsafonkin@gmail.com>
2020-11-03 12:10:29 +0300

    Test

--- images/macos/provision/utils/xamarin-utils.sh
+++ images/macos/provision/utils/xamarin-utils.sh
@@ -193,9 +193,10 @@ installNuget() {
   echo "Installing nuget $NUGET_VERSION for Mono $MONO_VERSION"
   cd ${MONO_VERSIONS_PATH}/${MONO_VERSION}/lib/mono/nuget
 
+  sudo mv nuget.exe nuget_old.exe
+
   pushd $TMPMOUNT
 
-  sudo mv nuget.exe nuget_old.exe
   download_with_retries $NUGET_URL "." "nuget.exe"
   # sudo mv /tmp/nuget.exe $TMPMOUNT
   sudo chmod a+x nuget.exe
